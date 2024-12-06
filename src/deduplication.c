bad964c029dd9d359f9b8e66f47e84ff0458e7b1
Th0mcote <thomas.pardaille@gmail.com>
2024-12-06 11:14:44 +0100

    Fonction add_md5 , find_md5
    J'ai fais ces fonctions mais pas sur que cela fonctionne

--- src/deduplication.c
+++ src/deduplication.c
@@ -15,10 +15,10 @@ unsigned int hash_md5(unsigned char *md5) {
     }
     return hash % HASH_TABLE_SIZE;
 }
-//blusk le goat
-//pierre n'est pas le goat
+
 // Fonction pour calculer le MD5 d'un chunk
 void compute_md5(void *data, size_t len, unsigned char *md5_out) {
+
 }
 
 // Fonction permettant de chercher un MD5 dans la table de hachage
@@ -28,16 +28,22 @@ int find_md5(Md5Entry *hash_table, unsigned char *md5) {
     *  @return: retourne l'index s'il trouve le md5 dans le tableau et -1 sinon
     */
     int resultat = -1;
-    for (int i=0;i<HASH_TABLE_SIZE;i++){
+    int i = 0
+    while (resultat == -1 && i < HASH_TABLE_SIZE){
         if (strcmp(hash_table[i].md5,md5) == 0){
             resultat = hash_table[i].index;
         }
+        i++
     }
     return resultat;
 }
 
 // Ajouter un MD5 dans la table de hachage
 void add_md5(Md5Entry *hash_table, unsigned char *md5, int index) {
+    for (int i = 0; i < MD5_DIGEST_LENGTH; i++) {
+        hash_table[index].md5[i] = md5[i];
+    }
+    hash_table[index].index = index;
 }
 
 // Fonction pour convertir un fichier non dédupliqué en tableau de chunks
