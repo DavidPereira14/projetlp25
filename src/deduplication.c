b94a6857e49f605f8c9bd54c11bc15443e968002
Th0mcote <thomas.pardaille@gmail.com>
2024-12-06 10:43:52 +0100

    Fonction find_md5 faite

--- src/deduplication.c
+++ src/deduplication.c
@@ -27,7 +27,13 @@ int find_md5(Md5Entry *hash_table, unsigned char *md5) {
     *           md5 est le md5 du chunk dont on veut déterminer l'unicité
     *  @return: retourne l'index s'il trouve le md5 dans le tableau et -1 sinon
     */
-    
+    int resultat = -1;
+    for (int i=0;i<HASH_TABLE_SIZE;i++){
+        if (strcmp(hash_table[i].md5,md5) == 0){
+            resultat = hash_table[i].index;
+        }
+    }
+    return resultat;
 }
 
 // Ajouter un MD5 dans la table de hachage
